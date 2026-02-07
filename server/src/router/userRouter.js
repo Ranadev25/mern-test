c1493b87d3762e1375c0fbfcfedb1e6694ece713
Ranadev25 <ranakhandev2025@gmail.com>
2026-02-07 14:29:05 +0600

    i leraned how to validation user with express-validation

--- server/src/router/userRouter.js
+++ server/src/router/userRouter.js
@@ -5,6 +5,8 @@ const registerUser = require("../controllers/registerControllers");
 const { userGet } = require("../controllers/userPaginationControllers");
 const activateUser = require("../controllers/verifyedUserController");
 const upload = require("../service/multer");
+const validateUserRegistration = require("../validation/auth");
+const runValidation = require("../validation/run");
 
 // get user by pagination
 router.get("/", userGet);
@@ -15,11 +17,9 @@ router.get("/:id", findUserById);
 // delete user by id
 router.delete("/:id", deleteUser);
 
-router.post("/register", upload.single("image"),registerUser);
-
+router.post("/register", upload.single("image"), validateUserRegistration,runValidation, registerUser);
 
 router.post("/verify", activateUser);
-
-
+// router.post("/signing");
 
 module.exports = router;
---
+++ server/src/validation/auth.js
@@ -0,0 +1,33 @@
+const { body } = require("express-validator");
+
+const validateUserRegistration = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3, max: 31 })
+    .withMessage("Name should be at least 3-31 characters long"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email address"),
+  body("password")
+    .trim()
+    .notEmpty()
+    // .matches(regax here with all characters)
+    .withMessage("password is required")
+    .isLength({ min: 6, max: 15 })
+    .withMessage("password should be at least 6-15 characters long"),
+  body("phone")
+    .trim()
+    .notEmpty()
+    .withMessage("required your bangladesh phone Number"),
+  body("image")
+    .optional()
+    .isString()
+    .withMessage("image is optional string")
+];
+
+module.exports = validateUserRegistration;
